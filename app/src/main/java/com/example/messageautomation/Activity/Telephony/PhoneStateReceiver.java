572a3c457729796dbb7579e5a671d0d1368c6965
007Nil <sagniksarkar@ymail.com>
2020-08-11 12:55:09 +0530

    Add incoming call sence functionality, will fetch the incoming number

---
+++ app/src/main/java/com/example/messageautomation/Activity/Telephony/PhoneStateReceiver.java
@@ -0,0 +1,29 @@
+package com.example.messageautomation.Activity.Telephony;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class PhoneStateReceiver extends BroadcastReceiver {
+
+    private String callState;
+    private String incomingNumber;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            callState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            if (callState.equals(TelephonyManager.EXTRA_STATE_RINGING) && incomingNumber != null){
+                Toast.makeText(context, "Ringing State Number is - " + incomingNumber, Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
